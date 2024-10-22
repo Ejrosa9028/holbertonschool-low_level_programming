ac6f65f3cd80aaa691f96581d7420b195e42593d
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-22 09:43:03 -0400

    puts hello #4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * File: 4-puts.c
+ * Author: Emanuel J. Rosa Alamo
+ * Date: October-22-2024
+ * Description: Este programa se usa el comando puts
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
