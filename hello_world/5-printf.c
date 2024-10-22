b5c1c2deb1da1ba9eeee7560892e378ce527f61f
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-22 10:19:49 -0400

    Printf hello

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints exactly "a piece of art,"
+ *        followed by a new line.
+ *
+ * Return: 0 on success
+ */
+
+int main(void) /* Declaro la funcion main*/
+{
+/*uso el comando printf para imprimir*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
