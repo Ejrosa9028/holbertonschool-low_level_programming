8aa98b88b97138a76ecbfeb1133a1f00899127e2
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-23 12:46:46 -0400

    Last number #3

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
 	}
-	else i (last_num == 0)
+	else if (last_num == 0)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, last_num);
 	}
