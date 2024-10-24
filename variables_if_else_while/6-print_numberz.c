2cd3d51180aca80712896b05a2b5173bb2c4b47b
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-24 11:09:54 -0400

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Generates a random number and prints whether it is positive,
+ *        negative, or zero.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+        int i = 0;
+        char num = '0';
+
+        while (i < 10)
+        {
+                putchar(num);
+                num++;
+
+                i++;
+        }
+
+        putchar('\n');
+
+        return (0);
+}
