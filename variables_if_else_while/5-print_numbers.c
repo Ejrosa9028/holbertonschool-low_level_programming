5e77558d7b830c878c6411b99255d1121698cf0f
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-24 10:46:01 -0400

    number

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Generates a random number and prints whether it is positive,
+ *        negative, or zero.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	for (int i = 0; i < 11; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
