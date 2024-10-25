4fb9d78029e09187d85effd1f69c0178256e1b65
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-25 11:08:42 -0400

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Generates a random number and prints whether it is positive,
+ *        negative, or zero.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
