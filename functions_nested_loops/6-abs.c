6c77715d2dd9215a71989ce79d9007778e09fdc1
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-26 17:21:53 -0400

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - checks if a character is lowercase
+ * @j: the character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _abs(int j)
+{
+
+	return (abs(j));
+}
