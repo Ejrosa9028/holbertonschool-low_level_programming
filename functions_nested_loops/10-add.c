fd7dc014c2307ab3ebf6edd1be42547013c798d7
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-27 20:36:42 -0400

    suma

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Imprime el ultimo digito de un numero
+ * @a: El numero del cual se extraera el ultimo digito
+ * @b: El numero del cual se extraera el ultimo digito
+ *
+ * Return: El valor del ultimo digito
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
