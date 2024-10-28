10544680df7ff637c39f507ac2516727015bd570
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-28 10:47:53 -0400

    multiplicacion

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Imprime si es uppercase o lowercase
+ * @a: recoge el numero del resultado
+ * @b: recoge el numero del resultado
+ *
+ * Return: El valor del ultimo digito
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
