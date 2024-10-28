260cdf740fadb5d0fdb3bfa93df51100a2ff9a87
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-28 09:56:03 -0400

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - Imprime si es uppercase o lowercase
+ * @c: recoge el numero del resultado
+ *
+ * Return: El valor del ultimo digito
+ */
+int _isupper(int c)
+{
+	if (c <= 'z' && c >= 'a')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
