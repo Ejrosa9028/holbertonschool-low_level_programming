64dfb5cc31d2bcbbe61cbe3b444b2efb40f5e336
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-10-30 10:49:26 -0400

    Baterry #2

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * reset_to_98 - Imprime si es Fizz o Buzz
+ *
+ * @n: recoge el numero del resultado
+ *
+ * Return: El valor del ultimo digito
+ */
 
 void reset_to_98(int *n)
 {
