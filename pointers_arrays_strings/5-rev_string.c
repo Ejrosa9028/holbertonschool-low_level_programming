740ab87bdff9f2462fa7c952d1957ac4699a6f91
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-11-01 11:24:22 -0400

    school

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c" /* Llamamos a la funcion 2-strlen.c*/
+
+/**
+ * rev_string - Imprime si es Fizz o Buzz
+ *
+ * @s: recoge el numero del resultado
+ *
+ * Return: El valor del ultimo digito
+ */
+
+void rev_string(char *s)
+{
+	int length = _strlen(s); /*Estamos igualando length a la funcion _strlen(s)*/
+	int star = 0;
+	int end = length - 1;
+
+	while (star != end)
+	{
+		char tmp = s[star];
+
+		s[star] = s[end];
+		s[end] = tmp;
+
+		star++;
+		end--;
+	}
+}
