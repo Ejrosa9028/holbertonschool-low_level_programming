22731bdb90fe73ef7c5e576caf4515928668630c
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-11-04 12:38:29 -0400

    2. strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,22 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+ * _strchr - Imprime si es Fizz o Buzz
+ *
+ * @s: recoge el numero del resultado
+ *
+ * @c: recoge el numero del resultado
+ *
+ * Return: El valor del ultimo digito
+ */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
+	while (*s != '\0') {
+		if (*s == c) {
 			return s;
 		}
-
-		i++;
+		s++;
 	}
-
+	return (c == '\0') ? s : NULL;
 }
