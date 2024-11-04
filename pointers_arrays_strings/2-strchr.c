ebe5166d400bd23a2a94b598e4580d855ff56f29
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-11-04 12:46:57 -0400

    2. strchr #2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0') {
-		if (*s == c) {
-			return s;
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
 		}
 		s++;
 	}
-	return (c == '\0') ? s : NULL;
+
+	return ((c == '\0') ? s : NULL);
 }
