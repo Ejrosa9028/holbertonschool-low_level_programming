af596d2c78973375434d9cdd271bcfdb5d51df68
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-11-13 10:01:57 -0400

    3. If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,6 +1,49 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * alloc_grid - copies the string pointed to by src
+ * @height: pointer to the destination buffer
+ * @width: pointer to the source string
+ *
+ * Return: pointer to dest
+ */
 int **alloc_grid(int width, int height)
 {
-	
+	int **tmp;
+	int i, j = 0;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	tmp = malloc(sizeof(int *) * height);
+
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+	{
+		tmp[i] = malloc(sizeof(int) * height);
+		if (tmp[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(tmp[j]);
+				free(tmp);
+				return (NULL);
+			}
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			tmp[i][j] = 0;
+		}
+	}
+
+	return (tmp);
 }
